2184012a655b949f116980455f3ecdea5edaf58b
simonebesutti23 <118028228+simonebesutti23@users.noreply.github.com>
2023-07-05 14:50:14 +0200

    finished

--- js/main.js
+++ js/main.js
@@ -17,4 +17,38 @@ var typed = new Typed(".auto-input" , {
     loop: true
 })
 
+const contactForm = document.querySelector("#form");
+const submitBtn = document.querySelector("#submit-btn");
+const nameInput = document.querySelector("#fullName");
+const emailInput = document.querySelector("#emailId");
+const messageInput = document.querySelector("#message");
 
+
+const publicKey = "ZOsENfQL-H8edUhqt";
+const serviceId = "service_6lch37a";
+const templateId = "template_upu0jsq"
+
+emailjs.init(publicKey);
+
+contactForm.addEventListener("submit" , e => {
+    e.preventDefault();
+    submitBtn.innerText = "Just a moment...."
+
+    const inputFields = {
+        name: nameInput.value,
+        email: emailInput.value,
+        message: messageInput.value
+    }
+
+
+    emailjs.send(serviceId, templateId, inputFields)
+    .then(() => {
+        submitBtn.innerText = "Message Sent Successfully";
+        nameInput.value = "";
+        emailInput.value = "";
+        messageInput.value = "";
+    }, (error) => {
+        console.log(error);
+        submitBtn.innerText = "Something went wrong"
+    });
+});
